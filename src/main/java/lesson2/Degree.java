e180006b74eb2439682c64eb8dac5dde59402524
Andrey <k-udm@ya.ru>
2023-11-06 13:05:03 +0100

    Lesson 3

---
+++ src/main/java/lesson2/Degree.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public enum Degree {
+
+    BACHELOR("Бакалавр"),
+    MASTER("Магистр");
+
+    private String description;
+
+    Degree(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
